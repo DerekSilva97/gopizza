285274adda4053dde1d31511f98c11e3878789cb
Derek <dereksilva194@gmail.com>
2022-09-08 19:39:54 -0300

    feito configurações para o firebase e feito tela de login com component Input

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
-import {  ThemeProvider } from 'styled-components/native';
+import { ThemeProvider } from 'styled-components/native';
 
 import theme from './src/theme';
 
@@ -17,7 +17,13 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {!fontsLoaded ? <Loading/> : <SignIn/>}
+      <StatusBar
+        style='light'
+        translucent
+        backgroundColor='transparent'
+      />
+
+        {!fontsLoaded ? <Loading /> : <SignIn />}
     </ThemeProvider>
   );
 }
